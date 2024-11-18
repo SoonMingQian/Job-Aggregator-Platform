abb778aa318d2d27e4bcfb4100abe19947411432
G00438454 <g00438454@atu.ie>
2024-11-18 11:44:34 +0000

    add models jobs

--- jobs-storage/jobs-storage-service/src/main/java/com/example/storage/models/Jobs.java
+++ jobs-storage/jobs-storage-service/src/main/java/com/example/storage/models/Jobs.java
@@ -5,4 +5,68 @@ import jakarta.persistence.*;
 @Entity
 public class Jobs {
 	
+	@Id
+	private String jobId;
+	
+	private String title;
+	
+	private String company;
+	
+	private String location;
+	
+	@Lob
+	private String applyLink;
+	
+	@Lob 
+	private String jobDescription;
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getApplyLink() {
+		return applyLink;
+	}
+
+	public void setApplyLink(String applyLink) {
+		this.applyLink = applyLink;
+	}
+
+	public String getJobDescription() {
+		return jobDescription;
+	}
+
+	public void setJobDescription(String jobDescription) {
+		this.jobDescription = jobDescription;
+	}
+	
+	
 }
