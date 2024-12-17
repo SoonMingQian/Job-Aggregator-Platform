f59fff970aa94e288a91825a1a36d374c471390c
G00438454 <g00438454@atu.ie>
2024-12-17 18:38:08 +0000

    add user management service

---
+++ user-management-service/src/main/java/com/example/user/models/User.java
@@ -0,0 +1,62 @@
+package com.example.user.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id 
+	private String id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ user-management-service/src/main/java/com/example/user/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.user.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.user.models.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+	Optional<User> findOptionalByEmail(String email);
+	
+	Boolean existsByEmail(String email);
+}
