b7d379cc7327cda1fcfe9dbd843d2fff6c03ebc9
G00438454 <g00438454@atu.ie>
2024-12-18 01:41:48 +0000

    dockerize application

--- jobs-storage/jobs-storage-service/src/test/java/com/example/demo/JobsStorageServiceApplicationTests.java
+++ jobs-storage/jobs-storage-service/src/test/java/com/example/demo/JobsStorageServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.storage.JobsStorageServiceApplication;
+
+@SpringBootTest(classes = JobsStorageServiceApplication.class)
 class JobsStorageServiceApplicationTests {
 
 	@Test
