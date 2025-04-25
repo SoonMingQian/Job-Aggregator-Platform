93ad7b0914f1d7701e5ffb559c990497a07b8bdb
G00438454 <g00438454@atu.ie>
2025-04-25 15:45:18 +0100

    update origin

--- user-management-service/src/main/java/com/example/user/security/WebSecurityConfig.java
+++ user-management-service/src/main/java/com/example/user/security/WebSecurityConfig.java
@@ -81,7 +81,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://soonmingqian.github.io/Job-Aggregator-Platform", "http://localhost:8081"));
+		configuration.setAllowedOrigins(Arrays.asList("https://soonmingqian.github.io", "http://localhost:8081"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
